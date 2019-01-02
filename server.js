b3954573f3d7cf1cf5ec929eaa3a97c265572a67
dvld <nikoroberts620@gmail.com>
2019-01-01 19:57:36 -0600

    wrote server js file

---
+++ server.js
@@ -0,0 +1,24 @@
+
+// dependencies
+var express = require('express');
+var path = require('path');
+
+// __________
+
+// config
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// __________
+
+// listener
+app.listen(PORT, function () {
+  console.log('Server listening on port: ' + PORT);
+});
