f2d59f249a418f04f7a1c29d779227827eb73d0b
dvld <nikoroberts620@gmail.com>
2019-01-03 15:12:48 -0600

    added list of friends, photos, and scores

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+
+// data
+
+var friends = [
+
+  {
+    name: 'Steven',
+    photo: 'https://orig07.deviantart.net/255e/f/2015/231/6/0/steven_text_by_stevenquartzuniverse-d96c2nt.png',
+    scores: [
+      '4',
+      '1',
+      '3',
+      '2',
+      '3',
+      '5',
+      '4',
+      '2',
+      '2',
+      '1'
+    ]
+  }, {
+    name: 'Jake',
+    photo: 'https://logos.flamingtext.com/Name-Logos/Jake-design-china-name.png',
+    scores: [
+      '2',
+      '5',
+      '3',
+      '1',
+      '3',
+      '2',
+      '3',
+      '2',
+      '4',
+      '5'
+    ]
+  }, {
+    name: 'Bobby',
+    photo: 'http://www.morningkids.net/name/images/coloring-page-first-name-bobby.jpg',
+    scores: [
+      '1',
+      '1',
+      '5',
+      '4',
+      '3',
+      '2',
+      '3',
+      '4',
+      '4',
+      '2'
+    ]
+  }, {
+    name: 'Danielle',
+    photo: 'https://image.shutterstock.com/image-vector/danielle-female-name-street-art-260nw-423936919.jpg',
+    scores: [
+      '5',
+      '4',
+      '3',
+      '4',
+      '3',
+      '5',
+      '1',
+      '3',
+      '3',
+      '1'
+    ]
+  }, {
+    name: 'Wendy',
+    photo: 'http://www.morningkids.net/name/images/coloring-page-first-name-wendy.jpg',
+    scores: [
+      '2',
+      '1',
+      '3',
+      '1',
+      '3',
+      '2',
+      '1',
+      '4',
+      '1',
+      '2'
+    ]
+  }
+
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // require('./app/routing/apiRoutes')(app);
-// require('./app/routing/htmlRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
 // __________
 
