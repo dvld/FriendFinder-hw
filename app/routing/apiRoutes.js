91339ab5d0b0eedb3d2a6263bd9f76acb4e823a7
dvld <nikoroberts620@gmail.com>
2019-01-03 15:34:46 -0600

    added api routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,53 @@
+
+// load data from friends.js
+
+var friends = require('../data/friends');
+
+// __________
+
+// routing
+
+module.exports = function (app) {
+
+  app.get('/api/friends', function (req, res) {
+    res.json(friends);
+  });
+
+  app.post('/api/friends', function (req, res) {
+
+    var bestFriend = {
+      name: '',
+      photo: '',
+      difference: Infinity
+    };
+
+    var userData = req.body;
+    var userScores = userData.scores;
+
+    var totalDifference;
+
+    for (var i = 0; i < friends.length; i++) {
+      var currentFriend = friends[i];
+      totalDifference = 0;
+
+      console.log(currentFriend.name);
+
+      for (var j = 0; j < currentFriend.scores.length; j++) {
+        var currentFriendScore = currentFriend.scores[j];
+        var currentUserScore = userScores[j];
+
+        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+      }
+
+      if (totalDifference <= bestFriend.difference) {
+        bestFriend.name = currentFriend.name;
+        bestFriend.photo = currentFriend.photo;
+        bestFriend.difference = totalDifference;
+      }
+    }
+
+    friends.push(userData);
+
+    res.json(bestFriend);
+  });
+};
