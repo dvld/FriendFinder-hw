7b9f6d837c593c0a0f4c8e656224f7d33ea03457
dvld <nikoroberts620@gmail.com>
2019-01-03 16:28:58 -0600

    added comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,45 +9,58 @@ var friends = require('../data/friends');
 
 module.exports = function (app) {
 
+  // gets json data
   app.get('/api/friends', function (req, res) {
     res.json(friends);
   });
 
+  // user submitted form data pushed to js array
   app.post('/api/friends', function (req, res) {
 
+    // best overall match object
     var bestFriend = {
       name: '',
       photo: '',
       difference: Infinity
     };
 
+    // parse user results from survey POST
     var userData = req.body;
     var userScores = userData.scores;
 
+    // difference between user's score and users in database
     var totalDifference;
 
+    // loop throough all possibilities in the database
     for (var i = 0; i < friends.length; i++) {
       var currentFriend = friends[i];
       totalDifference = 0;
 
       console.log(currentFriend.name);
 
+      // loop through all scores
       for (var j = 0; j < currentFriend.scores.length; j++) {
         var currentFriendScore = currentFriend.scores[j];
         var currentUserScore = userScores[j];
 
+        // calculate difference between scores and sum into totalDifference
         totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
       }
 
+      // if the sum of differences is less than the differences of current match
       if (totalDifference <= bestFriend.difference) {
+
+        // reset bestFriend to the new match
         bestFriend.name = currentFriend.name;
         bestFriend.photo = currentFriend.photo;
         bestFriend.difference = totalDifference;
       }
     }
 
+    // save the user's data to the database (after checking for matches so the user is not returned as their own best match)
     friends.push(userData);
 
+    // return user's new friend's json data
     res.json(bestFriend);
   });
 };
